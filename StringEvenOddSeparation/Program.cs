330f385706e478660c63a40cb566c28ada983f35
aarthi <aarthi.saba@gmail.com>
2020-05-12 17:19:01 -0400

    first commit of valid solution

--- StringEvenOddSeparation/Program.cs
+++ StringEvenOddSeparation/Program.cs
@@ -15,6 +15,7 @@ namespace StringEvenOddSeparation
             {
                 inputStrings.Add(Console.ReadLine());
             }
+            Console.WriteLine("Output");
             foreach(var item in inputStrings)
             {
                 string evenletters = null,oddletters = null;
